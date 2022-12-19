54df6eb5f81a0c2df2247f78ad170fb91d258689
takelavr <96823498+takelavr@users.noreply.github.com>
2022-12-19 11:12:48 +0400

    Add files via upload

---
+++ Shaurma_JPA/src/main/java/shaurma_house/data/JpaOrderRepository.java
@@ -0,0 +1,18 @@
+package shaurma_house.data;
+
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import shaurma_house.model.Order;
+import shaurma_house.model.User;
+
+
+@Repository
+public interface JpaOrderRepository extends CrudRepository<Order, Long> {
+	
+	List<Order> findByUserOrderByPlacedAtDesc (User user, Pageable pagenable);
+	
+	
+}
