7193d899e7dd9741ce4ac621c29f4ef0d99e4036
takelavr <96823498+takelavr@users.noreply.github.com>
2022-12-23 10:07:31 +0400

    Delete Library directory

--- Library/src/main/java/rogovig/Library/data/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package rogovig.Library.data;
-
-import org.springframework.data.repository.CrudRepository;
-import rogovig.Library.model.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
--- Library/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into book (author, name) values('Пушкин','Капитанская дочка');
-insert into book (author, name) values('Лермонтов','Бородино');
-insert into book (author, name) values('Гоголь','Мертвые души');
-insert into book (author, name) values('Пелевин','Чапаев и пустота');
