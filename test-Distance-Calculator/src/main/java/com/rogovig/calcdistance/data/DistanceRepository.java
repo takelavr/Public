3395d2ccf7e311a78b71dcfce4c77550a449dd5b
takelavr <96823498+takelavr@users.noreply.github.com>
2022-12-28 14:43:26 +0400

    Delete test-Distance-Calculator directory

--- test-Distance-Calculator/src/main/java/com/rogovig/calcdistance/data/DistanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rogovig.calcdistance.data;
-
-import com.rogovig.calcdistance.model.Distance;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface DistanceRepository extends CrudRepository<Distance,Long> {
-}
